73d2e5d93dfca84f1c9d6e7838442a37519cd06a
Atul <atulrawatk@gmail.com>
2021-07-08 11:54:34 +0530

    Pin Page 1

--- lib/app/utils/styles/dimens.dart
+++ lib/app/utils/styles/dimens.dart
@@ -1,3 +1,14 @@
-class AppDimensions{
+import 'package:get/get.dart';
 
+import '';
+class AppDimensions{
+  static final double bigHeadingSize= Get.height/50;
+  static final double headingSize = Get.height/55;
+  static final double small_heading_size = 20.0;
+  static final double text_size = 16.0;
+  static final double small_text_size = 14.0;
+  static final double very_small_text_size = 12.0;
+  static final double vv_small_text_size = 10.0;
+  static final double big_icon_size = 32.0;
+  static final double icon_size = 24.0;
 }
